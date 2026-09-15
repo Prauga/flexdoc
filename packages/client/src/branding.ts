61754656af2f88f92077a28024f758cc013542c3
Vishnu R <45818225+bluejeans117@users.noreply.github.com>
2026-09-16 02:17:50 +0530

    fix: client/branding: use public FlexDoc asset origin
    Serve default renderer branding from the public FlexDoc website instead of the private website repository's raw GitHub endpoint.

--- packages/client/src/branding.ts
+++ packages/client/src/branding.ts
@@ -1,4 +1,4 @@
-const FLEXDOC_BRAND_ROOT = 'https://raw.githubusercontent.com/Prauga/flexdoc-website/main/public/brand/flexdoc';
+const FLEXDOC_BRAND_ROOT = 'https://flexdoc.prauga.com/brand/flexdoc';
 
 /** Theme-neutral FlexDoc mark used for default renderer branding and favicon. */
 export const FLEXDOC_MARK_URL = `${FLEXDOC_BRAND_ROOT}/favicon.svg`;
